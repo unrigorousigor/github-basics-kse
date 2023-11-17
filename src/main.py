e2a451cee54c112ee98a97da59db89c5cfce4969
unrigorousigor <143851420+unrigorousigor@users.noreply.github.com>
2023-11-17 02:48:00 +0000

    Added option to keep asking for the guess

--- src/main.py
+++ src/main.py
@@ -29,14 +29,16 @@ print(f"Your word is '{word}'")
 
 
 while not is_game_over():
-    guess = input("Your next take: ")
-    if guess in full_list:
-        guessed += 1
-        guesses.append(guess)
-        if guessed == WORDS_TO_WIN:
-            congratulate_user()
-            exit()
-        print(f"That's right! {WORDS_TO_WIN - guessed} to go")
-    else:
-        errors += 1
-        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+  guess = input("Your next take: ")
+  if has_word_been_guessed(guess):
+    print("You've already used that word. Try again.")
+  elif guess in full_list:
+    guessed += 1
+    guesses.append(guess)
+    if guessed == WORDS_TO_WIN:
+        congratulate_user()
+        exit()
+      print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+  else:
+    errors += 1
+    print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
